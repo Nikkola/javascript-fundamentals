290df6f19aa4357a18e52a27a8d213447b9bfc01
Nikkola <nicolachernobaev@gmail.com>
2013-09-23 00:31:37 +0400

    dom and bom

---
+++ 15 - window object/main.js
@@ -0,0 +1,22 @@
+var foo = 'String';
+
+// console.log( navigator.userAgent );
+// console.log( navigator.platform );
+
+//console.log( screen.width + ' x ' +screen.height );
+
+//console.log( location.toString() );
+
+//console.log( frames );
+
+//console.log( history );
+
+
+//window.confirm('BOM');
+var bar = window.prompt();
+
+if (bar === 'Привет') {
+	alert('Привет и тебе от BOM!')
+} else {
+	alert('Пока!');
+}
